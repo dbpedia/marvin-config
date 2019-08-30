de2a2a49bcf46731da09179713227bdb2c93ff57
Root <kurzum@github.com>
2019-08-30 22:51:04 +0200

    working

--- marvin-fetch.sh
+++ marvin-fetch.sh
@@ -2,13 +2,13 @@
 
 GROUP=$1
 VERSION=$2
+SERVER=$3
 
 # get artifacts
-ARTIFACTS=` xmlstarlet sel -N my=http://maven.apache.org/POM/4.0.0 -t -v "/my:project/my:modules/my:module" $GROUP/pom.xml`
+ARTIFACTS=`xmlstarlet sel -N my=http://maven.apache.org/POM/4.0.0 -t -v "/my:project/my:modules/my:module" $GROUP/pom.xml`
 
-#ARTIFACTS="geo-coordinates-mappingbased instance-types specific-mappingbased-properties mappingbased-literals mappingbased-objects-uncleaned"
-#VERSION="2019.08.01"
-for i in $ARTIFACTS ; do 
+for a in $ARTIFACTS ; do 
 echo $i 
-#scp -rv marvin-fetch@dbpedia-mappings.tib.eu:/data/databus-maven-plugin/dbpedia/mappings/$i/$VERSION $i/
+#scp -rv marvin-fetch@$SERVER:/data/databus-maven-plugin/dbpedia/$GROUP/$a/$VERSION $GROUP/$a/
+rsync -av --ignore-existing marvin-fetch@$SERVER:/data/databus-maven-plugin/dbpedia/$GROUP/$a/$VERSION $GROUP/$a/$VERSION
 done 
