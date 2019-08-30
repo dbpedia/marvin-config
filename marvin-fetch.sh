ac3100178b0cdd17b86c4381778b7d4e33f3908a
Root <kurzum@github.com>
2019-08-30 23:24:59 +0200

    rsync fix

--- marvin-fetch.sh
+++ marvin-fetch.sh
@@ -14,5 +14,5 @@ ARTIFACTS=`xmlstarlet sel -N my=http://maven.apache.org/POM/4.0.0 -t -v "/my:pro
 for a in $ARTIFACTS ; do 
 echo $i 
 #scp -rv marvin-fetch@$SERVER:/data/databus-maven-plugin/dbpedia/$GROUP/$a/$VERSION $GROUP/$a/
-rsync -av -e ssh --ignore-existing marvin-fetch@$SERVER:/data/databus-maven-plugin/dbpedia/$GROUP/$a/$VERSION $GROUP/$a/$VERSION
+rsync -av -e ssh --ignore-existing marvin-fetch@$SERVER:/data/databus-maven-plugin/dbpedia/$GROUP/$a/$VERSION $GROUP/$a
 done 
