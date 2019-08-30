f628e2fbaf3bfbae096aad0001c034b86ae5c5fc
Root <kurzum@github.com>
2019-08-30 23:08:30 +0200

    finished

--- marvin-fetch.sh
+++ marvin-fetch.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+
+# ./marvin-fetch.sh wikidata 2019.08.01 dbpedia-wikidata.tib.eu 
+
+
 GROUP=$1
 VERSION=$2
 SERVER=$3
@@ -10,5 +14,5 @@ ARTIFACTS=`xmlstarlet sel -N my=http://maven.apache.org/POM/4.0.0 -t -v "/my:pro
 for a in $ARTIFACTS ; do 
 echo $i 
 #scp -rv marvin-fetch@$SERVER:/data/databus-maven-plugin/dbpedia/$GROUP/$a/$VERSION $GROUP/$a/
-rsync -av --ignore-existing marvin-fetch@$SERVER:/data/databus-maven-plugin/dbpedia/$GROUP/$a/$VERSION $GROUP/$a/$VERSION
+rsync -av -e ssh --ignore-existing marvin-fetch@$SERVER:/data/databus-maven-plugin/dbpedia/$GROUP/$a/$VERSION $GROUP/$a/$VERSION
 done 
