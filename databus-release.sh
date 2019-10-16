c8c6df8bae02cd1aa5e0a08a348d34d8fcdcb2c5
Sebastian Hellmann <hellmann@informatik.uni-leipzig.de>
2019-10-16 11:03:55 +0200

    cleanup

--- databus-release.sh
+++ databus-release.sh
@@ -15,3 +15,17 @@ git pull
 
 # copy 
 
+
+# deploy 
+cd $DATABUSDIR;
+mvn versions:set -DnewVersion=$(ls * | grep '^[0-9]\{4\}.[0-9]\{2\}.[0-9]\{2\}$' | sort -u  | tail -1);
+
+RELEASEPUBLISHER="https://vehnem.github.io/webid.ttl#this";
+RELEASEPACKAGEDIR="/data/extraction/release/\${project.groupId}/\${project.artifactId}";
+RELEASEDOWNLOADURL="http://dbpedia-generic.tib.eu/release/\${project.groupId}/\${project.artifactId}/\${project.version}/";
+RELEASELABELPREFIX="(pre-release)";
+RELEASECOMMENTPREFIX="(MARVIN is the DBpedia bot, that runs the DBpedia Information Extraction Framework (DIEF) and releases the data as is, i.e. unparsed, unsorted, not redirected for debugging the software. After its releases, data is cleaned and persisted under the dbpedia account.)";
+
+>&2 mvn clean deploy -Ddatabus.publisher="$RELEASEPUBLISHER" -Ddatabus.packageDirectory="$RELEASEPACKAGEDIR" -Ddatabus.downloadUrlPath="$RELEASEDOWNLOADURL" -Ddatabus.labelPrefix="$RELEASELABELPREFIX" -Ddatabus.commentPrefix="$RELEASECOMMENTPREFIX";
+ 
+
