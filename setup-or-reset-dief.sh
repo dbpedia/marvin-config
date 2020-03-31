c0c2e48acc6fc2e9dfd1c3d3ddba6329a7399508
Your Name <you@example.com>
2020-04-01 00:43:55 +0200

    putting spark local dir in marvinextraction

--- setup-or-reset-dief.sh
+++ setup-or-reset-dief.sh
@@ -14,7 +14,7 @@ cd $DIEFDIR
 # concat universial props
 echo "base-dir=$EXTRACTIONBASEDIR" > $DIEFDIR/core/src/main/resources/universal.properties 
 echo "log-dir=$LOGDIR/extraction/" >> $DIEFDIR/core/src/main/resources/universal.properties  
-echo "spark-local-dir=$MARVINEXTRATIONDIR/spark.local.dir/">> $DIEFDIR/core/src/main/resources/universal.properties 
+echo "spark-local-dir=$MARVINEXTRACTIONDIR/spark.local.dir/">> $DIEFDIR/core/src/main/resources/universal.properties 
 cat $CONFIGDIR/universal.properties.template >> $DIEFDIR/core/src/main/resources/universal.properties 
 
 mvn clean install 2>&1 | tee $LOGDIR/installDIEF.log
