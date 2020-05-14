3046fe9f56245e6b9698458a01f66d0fd8f898d3
vehnem <vehnem@yahoo.de>
2020-05-14 11:54:18 +0200

    monthly cron script

---
+++ release-monthly-cron.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+source ./functions.sh;
+
+#################
+# check argument
+#################
+GROUP=$1;
+
+if [ "$GROUP" != "generic" ] && [ "$GROUP" != "mappings" ] && [ "$GROUP" != "test" ] && [ "$GROUP" != "wikidata" ] && [ "$GROUP" != "text" ] || [ -z "$GROUP" ]
+then
+    echo "$HELP";
+    exit 1;
+fi
+
+
+#################
+# MARVIN release
+#################
+cd $ROOT;
+
+./marvin_extraction_run.sh $GROUP;
+[ $GROUP = "generic" ] && ./marvin_extraction_run.sh $GROUP.en;
+
+./databus-release.sh $GROUP;
+
+
+#####################
+# DBpedia re-release
+#####################
+cd $DATABUSDIR/dbpedia/$GROUP;
+
+deriveVersion=$(ls * | grep '^[0-9]\{4\}.[0-9]\{2\}.[0-9]\{2\}$' | sort -u  | tail -1)
+
+mvn -Ddatabus.deriveversion=$deriveVersion clean databus-derive:clone \
+&& for i in `ls -d */` ; do cd $i ; mvn  clean -Pwebdav deploy ; cd .. ; done
