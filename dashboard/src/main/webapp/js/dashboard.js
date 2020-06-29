a2e960acc35eadce017584eb8920d238b73270c6
vehnem <vehnem@yahoo.de>
2020-06-29 16:40:12 +0200

    dashboard frontend design

--- dashboard/src/main/webapp/js/dashboard.js
+++ dashboard/src/main/webapp/js/dashboard.js
@@ -1,3 +1,10 @@
+/* anchor scroll */
+window.addEventListener("hashchange", function () {
+    this.console.log("#")
+    window.scrollTo(window.scrollX, window.scrollY + 100);
+});
+
+
 var [publisher, group, version] = window.location.pathname.split("/").slice(1,4)
 var showTotalStats = publisher ? false : true
 
