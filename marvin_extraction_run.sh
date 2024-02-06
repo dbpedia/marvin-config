cf5978887b917a48060ae2aa8ef14f0ea0ef11db
Milan Dojchinovski <dojcinovski.milan@gmail.com>
2024-02-06 14:43:13 +0100

    new dumps download urls, spotlight config

--- marvin_extraction_run.sh
+++ marvin_extraction_run.sh
@@ -15,7 +15,7 @@ source functions.sh
 #################
 GROUP=$1
 
-if [ "$GROUP" != "generic" ] && [ "$GROUP" != "generic.en" ] && [ "$GROUP" != "mappings" ] && [ "$GROUP" != "test" ] && [ "$GROUP" != "wikidata" ] && [ "$GROUP" != "sparktestgeneric" ] && [ "$GROUP" != "text" ]  || [ -z "$GROUP" ]
+if [ "$GROUP" != "generic" ] && [ "$GROUP" != "generic.en" ] && [ "$GROUP" != "mappings" ] && [ "$GROUP" != "test" ] && [ "$GROUP" != "wikidata" ] && [ "$GROUP" != "sparktestgeneric" ] && [ "$GROUP" != "text" ] && [  "$GROUP" != "generic.spotlight" ]  && [ -z "mappings.spotlight" ] || [ -z "$GROUP" ]
 then
     echo "$HELP"
     exit 1
